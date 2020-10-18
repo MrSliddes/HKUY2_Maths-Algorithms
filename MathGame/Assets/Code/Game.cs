c9c1d2ddf75f40ce870223b094332ddd675c8b94
MrSliddes <mrsliddes@outlook.com>
2020-10-18 13:34:26 +0200

    Added MathGame Unity Files

---
+++ MathGame/Assets/Code/Game.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static Game Instance
+    {
+        get; private set;
+    }
+
+    private Player player;
+
+    public List<Enemy> enemies;
+
+    private List<Projectile> projectiles;
+
+    private Texture2D pixel;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        pixel = new Texture2D(1, 1);
+        pixel.SetPixel(0, 0, Color.white);
+        pixel.Apply();
+
+        player = new Player();
+
+        enemies = new List<Enemy>();
+        for(int i = 0; i < 1; ++i)
+        {
+            enemies.Add(new Enemy(new DevMath.Vector2(Random.Range(.0f, Screen.width), Random.Range(.0f, Screen.height))));
+        }
+
+        projectiles = new List<Projectile>();
+
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60; // Anders is mn editor bezig op 3000 fps
+    }
+
+    private void OnGUI()
+    {
+        player?.Render();
+
+        enemies.ForEach(e => e.Render());
+
+        projectiles.ForEach(p => p.Render());
+
+        if(player == null)
+        {
+            //Use Sin to animate the colour of the text (GUI.color) between alpha 0.5 and 1.0
+            float a = Mathf.Clamp(0.5f * Mathf.Sin((2 * Mathf.PI * Time.time) / 2), 0, 0.5f);
+            //Debug.Log(a);
+            GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, 0.5f + a);
+            GUI.Label(new Rect(Screen.width * .5f - 50.0f, Screen.height * .5f - 10.0f, 100.0f, 100.0f), "YOU LOSE!");
+        }
+        else
+		{
+            if (Input.GetKey(KeyCode.Q))
+            {
+                
+
+                foreach (var enemy in enemies)
+                {
+                    var direction = enemy.Position - player.Position;
+                    var distance = direction.Magnitude;
+
+                    var a = DevMath.DevMath.RadToDeg(DevMath.Vector2.Angle(new DevMath.Vector2(0, 0), direction));
+                    GUIUtility.RotateAroundPivot(a, player.Position.ToUnity());
+
+                    GUI.color = Color.magenta;
+
+                    GUI.DrawTexture(new Rect(player.Position.x, player.Position.y, distance, 1), pixel);
+
+                    GUI.matrix = Matrix4x4.identity;
+
+                    GUI.color = Color.black;
+
+                    var dot = DevMath.Vector2.Dot(direction.Normalized, player.Direction);
+
+                    var textPos = player.Position + DevMath.Vector2.DirectionFromAngle(a) * (distance * .5f);
+                    GUI.Label(new Rect(textPos.x, textPos.y, 200.0f, 200.0f), $"Distance: {distance}\nAngle: {a}\nDot: {dot}");
+                }
+
+                GUI.color = Color.white;
+            }
+		}
+    }
+
+    public void CreateProjectile(DevMath.Vector2 position, DevMath.Vector2 direction, float startVelocity, float acceleration)
+    {
+        projectiles.Add(new Projectile(position, direction, startVelocity, acceleration));
+    }
+
+    private void ScreenShake()
+    {
+        //Implement screen shake with Sin + Matrices
+        //https://www.youtube.com/watch?v=idNcM1_BPOY
+        float a = 10f * Mathf.Sin((2 * Mathf.PI * Time.time) / 2); if(a < 0) a = 0;
+        //GUI.DragWindow(new Rect(0, 0 + a, Screen.width, Screen.height));
+        GUI.matrix = Matrix4x4.TRS(new Vector3(1 + a, 1 + a, 1), Quaternion.identity, new Vector3(1, 1, 1));
+    }
+
+    private void Update()
+    {
+        if (player == null) return;
+
+        // For screenshake testing
+        //ScreenShake();
+
+        player.Update();
+
+        enemies.ForEach(e => e.Update(player));
+
+        for (int i = projectiles.Count - 1; i >= 0; i--)
+        {
+            projectiles[i].Update();
+            if(projectiles[i].ShouldDie)
+            {
+                projectiles.RemoveAt(i);
+            }
+        }
+
+        foreach(Enemy e in enemies)
+        {
+            if(e.Circle.CollidesWith(player.Circle))
+            {
+                player = null;
+            }
+        }
+
+        for (int i = projectiles.Count - 1; i >= 0; i--)
+        {
+            for(int j = enemies.Count - 1; j >= 0; --j)
+            {
+                if(projectiles[i].Circle.CollidesWith(enemies[j].Circle))
+                {
+                    enemies.RemoveAt(j);
+                    projectiles.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ MathGame/Assets/Code/Player.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Player
+{
+    private Texture2D visual;
+    public DevMath.Vector2 Position
+    {
+        get { return Circle.Position; }
+        set { Circle.Position = value; }
+    }
+
+    public DevMath.Vector2 Direction => DevMath.Vector2.DirectionFromAngle(Rotation);
+
+    public DevMath.Circle Circle
+    {
+        get; private set;
+    }
+
+    private Texture2D pixel;
+    
+    public float Rotation
+    {
+        get; private set;
+    }
+
+    private readonly float moveSpeed = 0.9f;
+
+    private const float MAX_CHARGE_TIME = 1.0f;
+
+    private const float MIN_PROJECTILE_START_VELOCITY = .0f;
+    private const float MAX_PROJECTILE_START_VELOCITY = 10.0f;
+    private const float MIN_PROJECTILE_START_ACCELERATION = 10.0f;
+    private const float MAX_PROJECTILE_START_ACCELERATION = 20.0f;
+
+    private float chargeTime;
+
+    private DevMath.Rigidbody rigidbody;
+
+    public Player()
+    {
+        visual = Resources.Load<Texture2D>("pacman");
+
+        Circle = new DevMath.Circle();
+        Circle.Radius = visual.width * .5f;
+
+        pixel = new Texture2D(1, 1, TextureFormat.RGBA32, false);
+        pixel.SetPixel(0, 0, Color.white);
+        pixel.Apply();
+
+        Position = new DevMath.Vector2(Screen.width * .5f - visual.width * .5f, Screen.height * .5f - visual.height * .5f);
+
+        rigidbody = new DevMath.Rigidbody()
+        {
+            mass = 1.0f,
+            force = 150.0f,
+            dragCoefficient = .47f
+        };
+    }
+
+    public void Render()
+    {
+        GUIUtility.RotateAroundPivot(Rotation, Position.ToUnity());
+
+        GUI.DrawTexture(new Rect(Position.x - Circle.Radius, Position.y - Circle.Radius, visual.width, visual.height), visual);
+
+        float p = DevMath.DevMath.Clamp(chargeTime, .0f, MAX_CHARGE_TIME) / MAX_CHARGE_TIME;
+        float fireVelocity = DevMath.DevMath.Lerp(MIN_PROJECTILE_START_VELOCITY, MAX_PROJECTILE_START_VELOCITY, p);
+        float fireAcceleration = DevMath.DevMath.Lerp(MIN_PROJECTILE_START_ACCELERATION, MAX_PROJECTILE_START_ACCELERATION, p);
+
+        float distanceTraveled = DevMath.DevMath.DistanceTraveled(fireVelocity, fireAcceleration, Projectile.LIFETIME);
+
+        //Implementeer de Line class met de IntersectsWith(Circle) functie en gebruik deze om de lijn rood te kleuren wanneer je een enemy zou raken
+        DevMath.Circle lineC = new DevMath.Circle();
+        lineC.Position = new DevMath.Vector2(Position.x + distanceTraveled, Position.y); lineC.Radius = 32;
+        DevMath.Circle otherC = new DevMath.Circle(); otherC.Radius = 32;
+        if(Game.Instance.enemies[0] != null) otherC.Position = new DevMath.Vector2(Game.Instance.enemies[0].Position.x, Game.Instance.enemies[0].Position.y);
+        //Debug
+        GUI.color = Color.magenta;
+        //Debug GUI.DrawTexture(new Rect(new Vector2(lineC.Position.x, lineC.Position.y), new Vector2(32, 32)), pixel);
+        GUIUtility.RotateAroundPivot(-Rotation, Position.ToUnity()); // Ow shit dit werkt
+        //Debug GUI.DrawTexture(new Rect(new Vector2(otherC.Position.x, otherC.Position.y), new Vector2(32, 32)), pixel);
+        GUIUtility.RotateAroundPivot(Rotation, Position.ToUnity()); // Reset weer
+        // Super leuk dat als je wilt testen dat de enemy op je player spawned waardoor je gelijk dood bent.
+        if(lineC.CollidesWith(otherC)) // Werkt soms en soms niet 
+        {
+            GUI.color = Color.red;
+        }
+        else
+        {
+            GUI.color = Color.black;
+        }
+        GUI.DrawTexture(new Rect(Position.x, Position.y, distanceTraveled, 1.0f), pixel);
+
+        GUI.matrix = Matrix4x4.identity;
+    }
+
+    private void UpdatePhysics()
+    {
+        DevMath.Vector2 forceDirection = new DevMath.Vector2(-Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //Debug.Log(forceDirection.x + " " + forceDirection.y);
+        //Debug.Log(Time.deltaTime);
+        rigidbody.AddForce(forceDirection, Time.deltaTime); // Ik heb geen idee waarom, maar de rb beweegt met lichtsnelheid. Nu niet meer, waarom weet ik niet
+
+        Position += rigidbody.Velocity;
+    }
+
+    public void Update()
+    {
+        UpdatePhysics();
+
+        var mousePos = new DevMath.Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+        var mouseDir = (mousePos - Position).Normalized;
+
+        Rotation = DevMath.DevMath.RadToDeg(DevMath.Vector2.Angle(new DevMath.Vector2(.0f, .0f), mouseDir));
+
+        if(Input.GetMouseButton(0))
+        {
+            chargeTime += Time.deltaTime;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            float p = DevMath.DevMath.Clamp(chargeTime, .0f, MAX_CHARGE_TIME) / MAX_CHARGE_TIME;
+
+            Game.Instance.CreateProjectile(Position, Direction, DevMath.DevMath.Lerp(MIN_PROJECTILE_START_VELOCITY, MAX_PROJECTILE_START_VELOCITY, p), DevMath.DevMath.Lerp(MIN_PROJECTILE_START_ACCELERATION, MAX_PROJECTILE_START_ACCELERATION, p));
+
+            chargeTime = .0f;
+        }
+    }
+}
