5e72863f28e6626d2242fd66fa098aa986c5df3a
MrSliddes <59771866+MrSliddes@users.noreply.github.com>
2020-10-18 13:26:31 +0200

    DevMath Dll

---
+++ DevMath/DevMath/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMath
+{
+    public class Circle
+    {
+        public Vector2 Position
+        {
+            get; set;
+        }
+
+        public float Radius
+        {
+            get; set;
+        }
+
+        // Als de circle overlaps dan is er een collision
+        public bool CollidesWith(Circle circle)
+        {
+            var dx = circle.Position.x - this.Position.x;
+            var dy = circle.Position.y - this.Position.y;
+            var dist = Math.Sqrt(dx * dx + dy * dy);
+            return dist < circle.Radius + Radius;
+        }
+    }
+}
---
+++ DevMath/DevMath/Line.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMath
+{
+    public class Line
+    {
+        // Hier niks doen?
+
+        public Vector2 Position
+        {
+            get; set;
+        }
+
+        public Vector2 Direction
+        {
+            get; set;
+        }
+
+        public float Length
+        {
+            get; set;
+        }
+    }
+}
